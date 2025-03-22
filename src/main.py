f42cc486747f54c9335717f9266715a003d1ddd6
Didier Obamium <156076403+DidierObamium@users.noreply.github.com>
2025-03-23 00:57:31 +0100

    Added some more game logic: some keybinds (movement, refresh and quitting). Also added a mroe human-readable display.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import time
+import keyboard
 
 import utilities.index as util
 import player.index as player
@@ -10,7 +11,7 @@ if __name__ == "__main__":
 
     # Test zone, not game logic
     print("Hello, world!")
-    player1 = player.Player(100.0, 0, 0)
+    player1 = player.Player(100.0, 2, 2)
     world_data = [
             [0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0],
@@ -20,9 +21,55 @@ if __name__ == "__main__":
             ]
     
     while True:
+        # First, we take care of the back-end world. Then, we display it.
         if tick:
-            world_data[player1.xpos][player1.ypos] = "P"
+            world_data[player1.ypos][player1.xpos] = "P"
             # Always this at the end
             util.clearScreen()
             world.showWorldData(world_data)
+            world.showWorldMap(world_data)
             tick = False
+
+        # Key bindings
+        # 'Refresh' key
+        if keyboard.is_pressed('r'):
+            tick = True
+            time.sleep(1/8)
+        # 'Quit game' key
+        if keyboard.is_pressed('q'):
+            util.clearScreen()
+            print("Quitting...")
+            break
+        # All of the movement keys
+        # 'Move up' key
+        if keyboard.is_pressed('w'):
+            world_data[player1.ypos][player1.xpos] = 0 # Remove the player
+            player1.ypos = player1.ypos - 1 # Change xpos
+            if player1.ypos == -1: # If at the end of world, move to bottom
+                player1.ypos = (len(world_data) - 1)
+            tick = True
+            time.sleep(1/8)
+        # 'Move down' key
+        if keyboard.is_pressed('s'):
+            world_data[player1.ypos][player1.xpos] = 0 # Remove the player
+            player1.ypos = player1.ypos + 1 # Change xpos
+            if player1.ypos == len(world_data): # If at the end of world, move to top
+                player1.ypos = 0
+            tick = True
+            time.sleep(1/8)
+        # 'Move left' key
+        if keyboard.is_pressed('a'):
+            world_data[player1.ypos][player1.xpos] = 0 # Remove the player
+            player1.xpos = player1.xpos - 1 # Change xpos
+            if player1.xpos == -1: # If at the end of world, move to right
+                player1.xpos = (len(world_data[player1.ypos]) - 1)
+            tick = True
+            time.sleep(1/8)
+        # 'Move right' key
+        if keyboard.is_pressed('d'):
+            world_data[player1.ypos][player1.xpos] = 0 # Remove the player
+            player1.xpos = player1.xpos + 1 # Change xpos
+            if player1.xpos == len(world_data[player1.ypos]): # If at the end of world, move to right
+                player1.xpos = 0
+            tick = True
+            time.sleep(1/8)
--- src/world/index.py
+++ src/world/index.py
@@ -1,7 +1,30 @@
 def showWorldData(world_data:list[list]):
     """
-    Input: world:[list[list]], current world.
-    Prints the current worldmap to the screen, line by line.
+    Input: world:[list[list]], current world's data.
+    Prints the current world's datamap to the screen, line by line.
     """
     for index in range(len(world_data)):
         print(world_data[index])
+
+def showWorldMap(world_data:list[list]):
+    """
+    Input: world:[list[list]], current world's data.
+    Prints the current worldmap to the screen.
+    """
+    printed_world = [
+        ["□", "□", "□", "□", "□"],
+        ["□", "□", "□", "□", "□"],
+        ["□", "□", "□", "□", "□"],
+        ["□", "□", "□", "□", "□"],
+        ["□", "□", "□", "□", "□"]
+    ]
+    for i in range(len(world_data)):
+        for j in range(len(world_data[i])):
+            if world_data[i][j] == "P": # If the player is on that square
+                printed_world[i][j] = "▼"
+    shown_line = ""
+    for i in range(len(printed_world)):
+        for j in range(len(printed_world[i])):
+            shown_line += printed_world[i][j]
+        print(shown_line)
+        shown_line = ""
